307e9cd07d3e4d44e2733874fc64a18387d152c1
Marcel.Silva <marcelsilva.dev@gmail.com>
2021-11-17 22:08:15 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultandoCep/Program.cs
@@ -0,0 +1,28 @@
+﻿using Caelum.Stella.CSharp.Http;
+using System;
+using System.Net.Http;
+
+namespace ConsultandoCep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Digite um Cep Válido ");
+            string cep = Console.ReadLine();
+
+            // Consultando o Web Service do Via Cep 
+            ViaCEP viaCEP = new ViaCEP();
+            string enderecoJson = viaCEP.GetEnderecoJson(cep);
+            Console.WriteLine(enderecoJson);
+
+            //Trazendo apenas Logradouro e Bairro
+            var endereco = viaCEP.GetEndereco(cep);
+            Console.WriteLine(string.Format("Logradouro:{0}, Bairro: {1} ", endereco.Logradouro, endereco.Bairro));
+
+            Console.ReadKey();
+        }      
+
+    }
+
+}
